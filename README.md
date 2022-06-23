6148cbb62cc1ab1469e57edfe1f56495e4f23907
zhouhao <zhouhao@email.cn>
2022-06-23 12:18:10 +0800

    完善md文件

--- README.md
+++ README.md
@@ -37,3 +37,6 @@
 </template>
 
 ```
+
+## 项目效果
+![](https://s3.bmp.ovh/imgs/2022/06/22/5d7674d86087e09b.png)
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,6 +1,6 @@
 <script setup>
   import { ref } from 'vue'
-  
+  import licensePlate from '../../components/licensePlate'
   const licensePlateRef = ref()
 
   const handleClick = () => {
